d42701af09a70e5dacdb64f716bf227128bce47c
Douglas Martins <douglas.pac@gmail.com>
2017-08-05 16:01:44 -0300

    Exercicios Parte 1

--- src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
+++ src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
@@ -7,14 +7,15 @@ import java.util.*
 fun todoTask11(): Nothing = TODO(
     """
         Task 11.
-        When an object implements a SAM interface (one with a Single Abstract Method), you can pass a lambda instead.
-        Rewrite the previous example changing an object expression to a lambda.
+        When an object implements a SAM interface (one with a Single Abstract Method),
+        you can pass a lambda instead.
+        Rewrite the previous example changing an obj0ect expression to a lambda.
     """,
     documentation = doc11()
 )
 
 fun task11(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> todoTask11() })
+    Collections.sort(arrayList) { x, y -> y - x }
     return arrayList
 }
--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -13,23 +13,21 @@ fun test() {
 }
 
 fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
+        """
         Task 7.
         Rewrite JavaCode7.sendMessageToClient in Kotlin, using only one 'if' expression.
         Declarations of Client, PersonalInfo and Mailer are given below.
     """,
-    documentation = doc7(),
-    references = { JavaCode7().sendMessageToClient(client, message, mailer) }
+        documentation = doc7(),
+        references = { JavaCode7().sendMessageToClient(client, message, mailer) }
 )
 
-fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
-) {
-    todoTask7(client, message, mailer)
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    message?.let { msg -> client?.personalInfo?.email?.let { mailer.sendMessage(it, msg) }}
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 
 interface Mailer {
     fun sendMessage(email: String, message: String)

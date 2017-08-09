1ac33e1176b843d24bf77ed5afd592759506f64d
DOUGLAS MARTINS <1510028461@acad.dvuadmin.net>
2017-08-08 21:40:46 -0300

    36-39

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,6 @@
 package v_builders
 
 import util.TODO
-import java.util.*
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -19,6 +18,21 @@ fun buildStringExample(): String {
     }
 }
 
+fun <K, V> buildMap(build: HashMap<K, V>.() -> Unit): Map<K, V> {
+    val map = HashMap<K, V>()
+    map.build()
+    return map
+}
+
+fun task37(): Map<Int, String> {
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
+}
+
 fun todoTask37(): Nothing = TODO(
     """
         Task 37.
@@ -28,12 +42,4 @@ fun todoTask37(): Nothing = TODO(
     """
 )
 
-fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
-}
+
